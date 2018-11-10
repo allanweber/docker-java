535406a7ba62d09e9676b3e6c3922e184c2cae74
Allan Weber <a.cassianoweber@gmail.com>
2018-11-10 14:04:59 -0200

    rename

---
+++ second/src/main/java/com/allanweber/Dockerize/DockerizeApplication.java
@@ -0,0 +1,12 @@
+package com.allanweber.Dockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeApplication.class, args);
+	}
+}
---
+++ second/src/test/java/com/allanweber/Dockerize/DockerizeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.allanweber.Dockerize;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerizeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
